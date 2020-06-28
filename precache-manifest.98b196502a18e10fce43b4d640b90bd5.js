9f9d83dc7991221cebb2fc92c7c0374a50ea4a78
jannkasper <jann.kasper@gmail.com>
2020-06-28 15:23:16 +0200

    Updates

---
+++ precache-manifest.98b196502a18e10fce43b4d640b90bd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7ad2e808125c68bb6ff59a78c74bdc",
+    "url": "/reactTODO/index.html"
+  },
+  {
+    "revision": "4e4289279c04ad33e906",
+    "url": "/reactTODO/static/css/main.729e8b5c.chunk.css"
+  },
+  {
+    "revision": "3d7706eb780a47d670e7",
+    "url": "/reactTODO/static/js/2.5b2c9404.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactTODO/static/js/2.5b2c9404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4289279c04ad33e906",
+    "url": "/reactTODO/static/js/main.be26d189.chunk.js"
+  },
+  {
+    "revision": "0782deeb3b00cc95ab35",
+    "url": "/reactTODO/static/js/runtime-main.372d10a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTODO/static/media/logo.5d5d9eef.svg"
+  }
+]);
